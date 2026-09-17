2c9fae74ab5b0084beb57af2760bfb4bef9bb7bb
Vignesh G <vigneshjon25@gmail.com>
2026-09-17 20:06:17 +0530

    Use configured HTTPS API URL

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,10 +10,7 @@ interface Todo {
 }
 
 const api = axios.create({
-  baseURL:
-    typeof window !== 'undefined'
-      ? `${window.location.protocol}//${window.location.hostname}:8000`
-      : process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
